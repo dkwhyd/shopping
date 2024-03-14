97ea034525f9747eb9167682962dfa43f5315a91
dkwhyd <dikiwhyd99@gmail.com>
2024-03-14 14:05:24 +0700

    create screen shopping

---
+++ lib/widget/shopping.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:shopping/helper/dbhelper.dart';
+
+class Shopping extends StatefulWidget {
+  const Shopping({super.key});
+
+  @override
+  ShoppingState createState() => ShoppingState();
+}
+
+class ShoppingState extends State {
+  DbHelper helper = DbHelper();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
