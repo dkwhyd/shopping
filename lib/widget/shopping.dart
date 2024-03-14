af239c6cee94116c52611d362f2ffa6ae6bf3e85
dkwhyd <dikiwhyd99@gmail.com>
2024-03-14 14:45:30 +0700

    show list shopping

--- lib/widget/shopping.dart
+++ lib/widget/shopping.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping/helper/dbhelper.dart';
+import 'package:shopping/widget/items_screen.dart';
 
 class Shopping extends StatefulWidget {
   const Shopping({super.key});
@@ -10,9 +11,42 @@ class Shopping extends StatefulWidget {
 
 class ShoppingState extends State {
   DbHelper helper = DbHelper();
+  List? shoppingList;
+
+  @override
+  void initState() {
+    // helper.deleteListItems();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    showData();
+    return ListView.builder(
+        itemCount: (shoppingList != null) ? shoppingList!.length : 0,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text(shoppingList![index].name),
+            leading: CircleAvatar(
+              child: Text(shoppingList![index].priority.toString()),
+            ),
+            trailing:
+                IconButton(onPressed: () {}, icon: const Icon(Icons.edit)),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => ItemsScreen(shoppingList![index])));
+            },
+          );
+        });
+  }
+
+  Future showData() async {
+    await helper.openDb();
+    shoppingList = await helper.getLists();
+    setState(() {
+      shoppingList = shoppingList;
+    });
   }
 }
